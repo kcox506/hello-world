543376024fb611254185e006b4a7f5984bf88908
kcox506 <kcox0026@email.cpcc.edu>
2021-02-06 10:11:11 -0500

    Create HelloWorld copy.cpp

---
+++ HelloWorld copy.cpp
@@ -0,0 +1,12 @@
+// Hello World. Program modified, but able to revert to original version using GitHub Desktop.ù 
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	cout << "Hello World";
+	cout << "Program modified, but able to revert to original version using GitHub Desktop";
+	
+	return 0;
+}
