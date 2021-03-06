45e4a71093074afa74c3b96575974d502d4f53e2
kcox506 <kcox0026@email.cpcc.edu>
2021-03-05 20:44:11 -0500

    Create Project_Kevin_Cox.cpp
    Added code for getting user input.

---
+++ Project_Kevin_Cox.cpp
@@ -0,0 +1,21 @@
+// This program calculates the graphics quality the user will be able to run on a newly develooped video game based on the specifications of their computer's hardware.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int GPUspeed = 0, CPUspeed = 0, cores = 0, resolutionChoice = 0;
+float multiplier = 0.0, performanceScore = 0.0;
+string graphicsQuality = "", headerText = "Computer Hardware Graphics Quality Recommendation Tool";
+int resolutionMatrix[4][2] ={
+{1280, 720},
+{1920, 1080},
+{2560, 1440},
+{3840, 2160}
+};
+  
+return 0;
+}
+
