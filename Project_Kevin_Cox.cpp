9d27b41b7d7dd78b7f4554e52349b7796ed37096
kcox506 <kcox0026@email.cpcc.edu>
2021-03-05 20:50:05 -0500

    Update Project_Kevin_Cox.cpp
    Added code for outputting the information.

--- Project_Kevin_Cox.cpp
+++ Project_Kevin_Cox.cpp
@@ -15,7 +15,59 @@ int resolutionMatrix[4][2] ={
 {2560, 1440},
 {3840, 2160}
 };
-  
+cout << "Please enter the clock speed (in Megahertz) of your graphics card: ";
+cin >> GPUspeed;
+      
+cout << "Please enter the clock speed (in Megahertz) of your processor: ";
+cin >> CPUspeed;
+      
+cout << "Please enter the number of cores of your processor: ";
+cin >> cores;
+      
+//display resolutionMatrix
+cout << "What is the resolution of your monitor?" << endl;
+for(int i=0; i<4; i++)
+cout << i+1 << ". " << resolutionMatrix[i][0] << " x " << resolutionMatrix[i][1] << endl;
+cout<<"Please select from the options above: ";
+cin >> resolutionChoice;
+      
+switch(resolutionChoice){
+case 1 : multiplier = 1;
+break;
+case 2 : multiplier = 0.75;
+break;
+case 3 : multiplier = 0.55;
+break;
+case 4 : multiplier = 0.35;
+break;
+default : cout << "Wrong choice input!";
+exit(0);
+}
+      
+//calculate performanceScore
+//Performance Score = ((5 * GPU Clock Speed) + (Number of Cores * CPU Clock Speed)) * Multiplier
+performanceScore = ((5*GPUspeed) + (cores*CPUspeed))*multiplier;
+      
+//determine graphicsQuality
+if(performanceScore >= 17000)
+graphicsQuality="Ultra";
+else if(performanceScore >= 15000)
+graphicsQuality="High";
+else if(performanceScore >= 13000)
+graphicsQuality="Medium";
+else if(performanceScore >= 11000)
+graphicsQuality="Low";
+else
+graphicsQuality="Unable to play.";
+      
+//output results
+cout << headerText << endl;
+cout << "GPU Clock Speed: " << GPUspeed << " MHz" << endl;
+cout << "CPU Clock Speed: " << CPUspeed << " MHz" << endl;
+cout << "Number of cores: " << cores << endl;
+cout << "Monitor Resolution: " << resolutionMatrix[resolutionChoice-1][0] << " x " << resolutionMatrix[resolutionChoice-1][1] << endl;
+cout << "Performance Score: " << performanceScore << endl;
+cout << "Recommended Graphics Quality: " << graphicsQuality << endl;
 return 0;
 }
 
